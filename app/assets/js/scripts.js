77c16e8acf2f4606d7b726906ad346ee55c7f268
alexandraklein <ali.klein@deutschinc.com>
2017-07-22 15:05:08 -0700

    smoothed out animations

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -6,66 +6,57 @@
  * @version 1.0.5
  * Copyright 2017. MIT licensed.
  */
-(function ($, window, document, undefined) {
+Barba.Pjax.start();
+Barba.Prefetch.init();
 
-  'use strict';
+var speed = 750;
 
-  $(function () {
-    Barba.Pjax.start();
-    Barba.Prefetch.init();
+var transition = Barba.BaseTransition.extend({
+  start: function() {
+    Promise
+      .all([this.newContainerLoading, this.fadeOut()])
+      .then(this.fadeIn.bind(this));
 
-    var transition = Barba.BaseTransition.extend({
-      start: function() {
-        Promise
-          .all([this.newContainerLoading, this.fadeOut()])
-          .then(this.fadeIn.bind(this));
-      },
+    setTimeout(function() {
+      $('html, body').animate({ scrollTop: 0 }, speed / 2);
+    }, speed / 2)
+  },
 
-      fadeOut: function() {
-        var $elOld = $(this.oldContainer);
-        var $nextArticle = $elOld.find('.next');
-        var height = $nextArticle.height();
-        var translationValue = $nextArticle.offset().top + (height / 2);
+  fadeOut: function() {
+    var $elOld = $(this.oldContainer);
+    var $nextArticle = $elOld.find('.next');
+    var height = $nextArticle.height();
+    var translationValue = $nextArticle.offset().top + (height / 2);
 
-        $elOld.removeClass('transition-in')
-              .addClass('transition-out')
-              .find('.next').css({ 'transform': 'translate3d(0, -' + translationValue + 'px, 0)' });
+    $elOld.removeClass('transition-in')
+      .addClass('transition-out')
+      .find('.next').css({ 'transform': 'translate3d(0, -' + translationValue + 'px, 0)' });
 
-        $('html, body').animate({ scrollTop: 0 }, 400);
+    return $elOld.animate({ opacity: 1 }).promise();
+  },
 
-        return $elOld.animate({ opacity: 1 }).promise();
-      },
+  fadeIn: function() {
+    var _this = this;
+    var $elNew = $(this.newContainer);
 
-      fadeIn: function() {
-        var _this = this;
-        var $elNew = $(this.newContainer);
+    $(this.oldContainer).hide();
 
-        $(this.oldContainer).hide();
-
-        $elNew.css({
-          visibility : 'visible',
-          opacity : 1
-        });
-
-        $elNew.animate(
-          { opacity: 1 }
-          , 1000, function() {
-            _this.done();
-        });
-
-
-
-
-        $elNew.addClass('transition-in')
-              .removeClass('transition-out');
-
-      }
+    $elNew.css({
+      visibility : 'visible',
+      opacity : 1
     });
 
-    Barba.Pjax.getTransition = function() {
-      return transition;
-    };
+    $elNew.animate(
+      { opacity: 1 }
+      , speed, function() {
+        _this.done();
+      });
 
-  });
+    $elNew.addClass('transition-in')
+      .removeClass('transition-out');
+  }
+});
 
-})(jQuery, window, document);
+Barba.Pjax.getTransition = function() {
+  return transition;
+};
