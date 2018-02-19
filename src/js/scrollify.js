6fe191c5f60d44a2b3c5bc6737d274f31319bcce
alexandraklein <ali.klein@deutschinc.com>
2018-02-19 12:31:08 -0800

    scrollify init for homepage

---
+++ src/js/scrollify.js
@@ -0,0 +1,45 @@
+var settings = {
+  section : '.scroll-section',
+  easing: 'easeOutExpo',
+  scrollSpeed: 1000,
+  offset : 0,
+  standardScrollElements: 'body:not(.home)',
+  scrollbars: true,
+  setHeights: true,
+  overflowScroll: true,
+  updateHash: false,
+  touchScroll: true,
+  before:function(index) {
+    $('.scroll-section').eq(index)
+      .removeClass('active previous next')
+      .addClass('active')
+      .siblings('.scroll-section')
+      .removeClass('active');
+
+    $('.scroll-section').eq(index - 1)
+      .removeClass('active previous next')
+      .addClass('previous')
+      .siblings('.scroll-section')
+      .removeClass('previous');
+
+    $('.scroll-section').eq(index + 1)
+      .removeClass('active previous next')
+      .addClass('next')
+      .siblings('.scroll-section')
+      .removeClass('next');
+  }
+};
+
+if($('body').hasClass('home')) {
+  $.scrollify(settings);
+}
+
+$(document).on('homepageOnEnterCompleted', function() {
+  $.scrollify(settings);
+});
+
+$(document).on('homepageOnLeaveCompleted', function() {
+  $.scrollify.destroy();
+});
+
+
