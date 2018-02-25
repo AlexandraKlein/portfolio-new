c1c9db2227f625427624f039ec700ee394834fa2
alexandraklein <ali.klein@deutschinc.com>
2018-02-24 17:56:34 -0800

    nav hover effects + nav click fix

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -227,11 +227,22 @@ $(function () {
 });
 
 var $body = $('body');
-var $navToggle = $('.nav, nav ul li a');
+var $navToggle = $('.nav');
+var $navItem = $('nav ul li a');
 
 $navToggle.on('click', function() {
   $(this).toggleClass('is-closed');
+
   if(!$(this).hasClass('is-closed')) {
+    $body.addClass('is-open');
+  } else {
+    $body.removeClass('is-open');
+  }
+});
+
+$navItem.on('click', function() {
+  $navToggle.toggleClass('is-closed');
+  if(!$navToggle.hasClass('is-closed')) {
     $body.addClass('is-open');
   } else {
     $body.removeClass('is-open');
