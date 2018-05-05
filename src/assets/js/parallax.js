469ae2b1198eac89cf18d9f1c74f40466ca10907
alexandraklein <ali.klein@deutschinc.com>
2018-05-05 12:30:35 -0700

    removed mobile parallax on device orientation

--- src/assets/js/parallax.js
+++ src/assets/js/parallax.js
@@ -11,14 +11,11 @@ function parallax() {
     container_h = $container.height();
   });
 
-  $(window).on('mousemove deviceorientation', event => {
-    const isMobile = window.matchMedia('(max-width: 767px)').matches;
-    const pos_x = isMobile ? Math.round(event.originalEvent.gamma * 10) : event.pageX;
-    const pos_y = isMobile ? Math.round(event.originalEvent.beta * 10) : event.pageY;
+  $(window).on('mousemove', event => {
+    const pos_x = event.pageX;
+    const pos_y = event.pageY;
     const left = container_w / 2 - pos_x;
     const top = container_h / 2 - pos_y;
-
-    $('#deviceOrientation').text(`gamma: ${event.originalEvent.gamma} beta: ${event.originalEvent.beta}`);
 
     function translateTween(args) {
       TweenLite.to(args.el, 1, {
